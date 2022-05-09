8b5af5acc81336999ec96468fbc105138c4f131a
Bakhtawar-123 <82812762+Bakhtawar-123@users.noreply.github.com>
2022-05-09 07:28:27 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+print("welcome to python test")
+a=("print")
+b=input("_____('hello world')   ")
+if a==b:
+    print("right! congratulations")
+else:
+    print("sorry! you are wrong")
+a1=('#')
+b1=input("__This is a comment  ")
+if a1==b1:
+    print("right! congratulations")
+else:
+    print("sorry! you are wrong")
+a2=("int")
+b2=input("x = 5 print(type(x))_________   ")
+if a2==b2:
+    print("right! congratulations")
+else:
+    print("sorry! you are wrong")
+a3=("str")
+b3=input("x = 'Hello World' print(type(x)) -----   ")
+if a3==b3:
+    print("right! congratulations")
+else:
+    print("sorry! you are wrong")
+a4=("banana")
+b4=input("fruits = ['apple', 'banana', 'cherry'] print(fruits[1]) what will be the output?   ")
+if a4==b4:
+    print("right! congratulations")
+else:
+    print("sorry! you are wrong")
+print("Thank You! the quiz end")
